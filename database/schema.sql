9c5bbd29f19c143d961e36e97aa732007e847757
Michael van der Loo <michaeljvanderloo@gmail.com>
2022-02-06 20:10:15 -0500

    database/schema: start

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,20 @@
-CREATE TABLE document ();
+CREATE TABLE Document (
+    DocumentID INT PRIMARY KEY,
+    FilingYear INT,
+    CommissionFileNumber INT,
+    EntityName VARCHAR(128),
+    State CHAR(2),
+    IRSEmployerID INT,
+    Address VARCHAR(128),
+    ZipCode INT
+);
+
+CREATE TABLE DocumentSection (
+    DocumentID INT,
+    SectionName VARCHAR(16),
+    SectionText VARCHAR(1024)
+);
+
+CREATE TABLE EntityLabel (
+    
+);
