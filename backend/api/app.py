2d01b3fdc19fc5ab6c4fc849139cb59eea53233c
Michael van der Loo <michaeljvanderloo@gmail.com>
2022-02-08 13:46:07 -0500

    added example react app and vscode extensions

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+# from flask import Flask
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/hello/', methods=['GET', 'POST'])
-def welcome():
-    return "Hello World!"
+# @app.route('/hello/', methods=['GET', 'POST'])
+# def welcome():
+#     return "Hello World!"
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=105)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=105)
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
