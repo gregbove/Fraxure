86b74e34fb62c719e16fdcb5957a02f30bb0ae17
Michael van der Loo <michaeljvanderloo@gmail.com>
2022-02-08 11:37:51 -0500

    details to schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,4 +1,10 @@
-CREATE TABLE Document (
+
+
+-- contains header information of the document
+-- document sections referece the document entry and contain the actual text
+-- a document being present doesnt guarentee its sections are present
+DROP TABLE IF EXISTS DocumentHeader;
+CREATE TABLE DocumentHeader (
     DocumentID INT PRIMARY KEY,
     FilingYear INT,
     CommissionFileNumber INT,
@@ -9,16 +15,20 @@ CREATE TABLE Document (
     ZipCode INT
 );
 
+-- references the document header it belongs to
+-- contains the section name and the document text
+-- also contains tags like viewed, NER labelled, human approved
+DROP TABLE IF EXISTS DocumentSection;
 CREATE TABLE DocumentSection (
     DocumentID INT,
-    SectionName VARCHAR(16),
-    SectionText VARCHAR(1024)
+    SectionName TEXT(16),
+    SectionText TEXT(1024)
 );
 
+-- references a document section, and specifically a set of words within the section
+-- associates a tag with the words
+DROP TABLE IF EXISTS EntityLabel;
 CREATE TABLE EntityLabel (
     
 );
 
-CREATE TABLE Jobs (
-    
-);
