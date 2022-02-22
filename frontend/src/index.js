841e03c106f85753f45df3125c4484939d35802c
Michael van der Loo <michaeljvanderloo@gmail.com>
2022-02-21 21:37:30 -0500

    idk

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,19 +3,18 @@ import { render } from "react-dom";
 
 import ExampleComponent from "./components/ExampleComponent";
 
-function App() { 
+function App() {
   return <ExampleComponent />;
 }
 
-const rootElement = document.getElementById("root")
-render(<App />, rootElement)
+const rootElement = document.getElementById("root");
+render(<App />, rootElement);
 
 // https://stackoverflow.com/questions/41956465/how-to-create-multiple-page-app-using-react
-import { BrowserRouter } from 'react-router-dom';
-ReactDOM.render((
+import { BrowserRouter } from "react-router-dom";
+ReactDOM.render(
   <BrowserRouter>
     <App /> {/* The various pages will be displayed by the `Main` component. */}
-  </BrowserRouter>
-  ), document.getElementById('root')
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
