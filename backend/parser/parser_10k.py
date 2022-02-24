da5750e82cf270911eefb03be4e11b96ee392c57
Michael van der Loo <michaeljvanderloo@gmail.com>
2022-02-24 11:47:00 -0500

    parser stuff

---
+++ backend/parser/parser_10k.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+from fuzzywuzzy import fuzz
+
+soup = BeautifulSoup(
+    "<html><head><title>Test</title></head>" "<body><h1>Parse me!</h1></body></html>",
+)
+
+print(soup.get_text())
