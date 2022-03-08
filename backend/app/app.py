edb299149890058d6d66b5f5d216a1d66d2bf2b6
Michael van der Loo <michaeljvanderloo@gmail.com>
2022-03-08 12:08:26 -0500

    flask edits

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,19 +1,25 @@
 # https://towardsdatascience.com/build-deploy-a-react-flask-app-47a89a5d17d9
 
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, render_template
 
-# app = Flask(__name__, static_url_path="", static_folder="frontend/build")
-app = Flask(__name__)
+app = Flask(
+    "Fraxure", static_url_path="/page", static_folder="../frontend/build/static"
+)
 
 
 @app.route("/")
-def index():
-    return "Index Page"
+def serve_static_react():
+    return send_from_directory("index.html")
 
 
-@app.route("/hello")
-def hello():
-    return "Hello, World"
+# @app.route("/")
+# def index():
+#     return "Index Page"
+
+
+@app.route("/api/")
+def api_route():
+    return "this accesses the api"
 
 
 if __name__ == "__main__":
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,16 @@
-import React from "react"; 
+import React from "react";
 import "./styles.css";
-import Results from './Results'; 
+import Results from "./Results";
 import Search from "./Search";
-// import Search from './Search'; 
+// import Search from './Search';
 
 function App() {
-    return <div id="pagedisplay" className="App">  
-    <Search />   
-    <Results />   
-    </div> 
-    ; 
+  return (
+    <div id="pagedisplay" className="App">
+      <Search />
+      <Results />
+    </div>
+  );
 }
 
 export default App;
