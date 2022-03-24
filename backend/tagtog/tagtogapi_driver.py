b5b687e1aa8d010068d692e503fe085d11b9f7ea
Ryan <ryan.e.murphy9@gmail.com>
2022-03-24 11:46:29 -0400

    Created TagTog API

--- backend/tagtog/tagtogapi_driver.py
+++ backend/tagtog/tagtogapi_driver.py
@@ -8,10 +8,13 @@ def main():
     t = TagTogAPI('ryanmurf9', 'testapipassword123')
 
     #(WORKING) Importing by plaintext
-    #t.import_by_plaintext("TestProject", "HELLO WORLD!!")
+    #t.import_by_plaintext("TestProject", "HELLO WOORLD!!")
 
-    #(NOT WORKING) Importing by url
-    #t.import_by_url('TestProject', 'https://www.sec.gov/Archives/edgar/data/0000045012/000004501222000013/hal-20211231.htm')
+    #(WORKING) Importing by url
+    #t.import_by_url('TestProject', 'https://www.sec.gov/Archives/edgar/data/0000093410/000009341022000019/cvx-20211231.htm')
+    
+    #(NOT WORKING) Importing by pdf
+    #t.import_by_pdf("Test Project", "/home/ryanmurf9/Downloads/test_sec.pdf")
 
 if __name__ == "__main__":
     main()
